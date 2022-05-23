89d5e538a6a6ee07bddf55dde2bc8cbc855e6181
Ruby <55691511+okpc579@users.noreply.github.com>
2022-05-23 14:38:40 +0900

    first commit

---
+++ src/test/java/com/sideproject/spssearch/SpsSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.spssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpsSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
